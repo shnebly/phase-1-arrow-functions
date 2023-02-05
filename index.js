0161ad69f540f1da35b6812c9b9a524c914a3f9c
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-05 07:49:56 -0700

    completed arrow function testing

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(number) {
+    return (number / 100);
+}
+divide(2000);
+
+const square = number => (number * number);
+square(20);
+
+const add = (number1, number2) => (number1 + number2);
+add(10,14);
